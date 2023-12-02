20dd02e986d5f69e4f116e331033f0ed4f0af9c4
Jose Antonio Verdugo <joseantonioverdugoardila@gmail.com>
2023-12-02 23:05:37 +0100

    cambiado a everyMinute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('send-products-email')->dailyAt('22:30');
+        $schedule->command('send-products-email')->everyMinute();
     }
 
     /**
